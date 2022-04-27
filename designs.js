d26b73de1aba6e8c2dfce1daab6edb98b001dd53
Tech-Her0 <100468255+Tech-Her0@users.noreply.github.com>
2022-04-27 19:56:32 -0400

    Add files via upload

--- designs.js
+++ designs.js
@@ -1,33 +1,33 @@
 // Select color input
 // Select size input
-var height, width, color;
+var tall, wide, paint;
 // When size is submitted by the user, call makeGrid()
 $('#sizePicker').submit(function (event) {
     event.preventDefault();
-    height= $('#inputHeight').val();
-    width= $('#inputWidth').val();
-    makeGrid (height, width);
+    tall= $('#inputHeight').val();
+    wide= $('#inputWidth').val();
+    makeGrid (tall, wide);
 
 });
 
-function makeGrid(x,y) {
+function makeGrid(l,h) {
     $('tr').remove();
 // Your code goes here!
-    for (var i=1; i<=x; i++) {
+    for (var i=1; i<=l; i++) {
         $('#pixelCanvas').append('<tr id=table' + i + '></tr>');
-        for (var j =1; j <=y; j++) {
+        for (var j =1; j <=h; j++) {
             $('#table' + i).append('<td></td>');
     }
 }
 
 //add color to cells
 $('td').click (function cellColor(){
-    color = $('#colorPicker').val();
+    paint = $('#colorPicker').val();
 
     if ($(this).attr('style')) {
         $(this).removeAttr('style')
     } else {
-        $(this).attr('style', 'background-color:' + color);
+        $(this).attr('style', 'background-color:' + paint);
     }
 
 })
